779693c72f88194d6e12d92d32bedd61e6d2451b
bboolean <this@is.fake>
2024-07-22 15:48:23 -0400

    Set up greeting page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
-<template class="">
-  <div>Hello {{ name }}!</div>
+<template>
+  <div class="m-10">
+    <h1 class="text-4xl font-bold mb-5">What is this?</h1>
 
-  <div class="space-y-4">
-    <div class="w-96 bg-white shadow rounded">w-96</div>
-    <div class="w-80 bg-white shadow rounded">w-80</div>
-    <div class="w-72 bg-white shadow rounded">w-72</div>
-    <div class="w-64 bg-white shadow rounded">w-64</div>
-    <div class="w-60 bg-white shadow rounded">w-60</div>
-    <div class="w-56 bg-white shadow rounded">w-56</div>
-    <div class="w-52 bg-white shadow rounded">w-52</div>
-    <div class="w-48 bg-white shadow rounded">w-48</div>
+    <div class="my-5">Hello {{ name }}!</div>
+
+    <button class="button bg-sky-500">12</button>
   </div>
-
-  <button class="button bg-sky-500">12</button>
 </template>
 
 <script>
